1f9e3fbbf78554be11cd4bfbb5b46bb2241654b3
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-13 21:14:03 +0100

    Added c-piscine-shell-01

--- main.sh
+++ main.sh
@@ -2,16 +2,21 @@
 
 # GLOBAL VARS
 languagesDir="languages/"
-secret=".secret/"
 currentLanguage="en"
+tmp="NULL" # This is used because I cannot return variables in bash
 
 
 main () {
 	chooseLanguage
-	echo $currentLanguage
+	chooseExercise
+	$tmp
 }
 
+# MENUS AND SELECTION STUFF, THESE COMMANDS ARE ALL THE SAME
 chooseLanguage () {
+	# If I could I would make a function to list an array and make a selection so I can resuse this code super easily
+	# But bash sucks ass a bit with storing variables and returning variable
+
 	# Store all json files into an array
 	local files=($(find $languagesDir -name "*.json" -type f -exec basename {} .json ";"))
 	local fileIndex=1
@@ -34,6 +39,37 @@ chooseLanguage () {
 		currentLanguage=${files[((languageIndex)) - 1]}
 	else
 		echo $(readJSON "invalidLangSelection")
+		chooseLanguage
+	fi
+}
+
+chooseExercise () {
+	# If I could I would make a function to list an array and make a selection so I can resuse this code super easily
+	# But bash sucks ass a bit with storing variables and returning variable
+
+	# Store all json files into an array
+	local files=($(find ".secret/" -name "c-piscine*" -type d -exec basename {} ";"))
+	local fileIndex=1
+
+	# Loop through language files and print them out
+	for languageName in "${files[@]}"
+	do
+		echo $fileIndex $languageName
+		fileIndex=$(($fileIndex + 1))
+	done
+
+	# Ask user for language selection
+	echo -n "$(readJSON chooseExercise) "
+	read languageIndex
+
+	# If language is valid, set the global language variable, if not, say its invalid
+	if (( $((languageIndex)) <=  fileIndex - 1 && $((languageIndex)) >= 1 ));
+	then
+		# Set the language
+		tmp=${files[((languageIndex)) - 1]}
+	else
+		echo $(readJSON "invalidLangSelection")
+		chooseExercise
 	fi
 }
 
