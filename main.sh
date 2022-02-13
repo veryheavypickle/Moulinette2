3558f0cb6310300853c38d3f5f6a6b9ce10fba9d
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-13 19:19:53 +0100

    built basic language selection

--- main.sh
+++ main.sh
@@ -5,20 +5,42 @@ languagesDir="languages/"
 secret=".secret/"
 currentLanguage="en"
 
+
+main () {
+	chooseLanguage
+}
+
 chooseLanguage () {
-	files=$(find $languagesDir -name "*.json" -type f -exec basename {} .json ";")
-	files=($ls $languagesDir) | sed "s/ /\n/g"
-	fileIndex=1
+	local files=$(find $languagesDir -name "*.json" -type f -exec basename {} .json ";")
+	local fileIndex=1
 	for languageName in "${files[@]}"
 	do
-		echo $langageName
+		echo $fileIndex $languageName
+		fileIndex=$(($fileIndex + 1))
 	done
+
+	echo -n "$(readJSON chooseLanguage) "
+	read languageIndex
+
+	if (( $((languageIndex)) <=  fileIndex - 1 && $((languageIndex)) >= 1 ));
+	then
+		echo "just correct"
+	fi
+
+	echo $languageIndex
 }
 
 readJSON () {
-	local filename="$languagesDir$currentLanguage.json"
 	local fieldName=$1
+
+	# If $2 is an empty variable, set it as the default language
+	if [ -z "$2" ]
+	then
+    	filename="$languagesDir$currentLanguage.json"
+	else
+		filename="$languagesDir$2.json"
+	fi
 	grep -o '"'$fieldName'": "[^"]*' $filename | grep -o '[^"]*$'
 }
 
-chooseLanguage
+main
